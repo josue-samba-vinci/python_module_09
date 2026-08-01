7d8737c1972bdc09f9d0cdee589ec4178a369aa9
Josue Samba Dianzungu <josuesamba99@gmail.com>
2026-08-01 17:45:18 +0200

    main to test Alien Contacts, valid and unvalid

--- ex1/alien_contact.py
+++ ex1/alien_contact.py
@@ -23,7 +23,7 @@ class AlienContact(BaseModel):
     is_verified: bool = False
 
     @model_validator(mode='after')
-    def validate_data(self) -> bool:
+    def validate_data(self) -> "AlienContact":
         if not self.contact_id.startswith("AC"):
             raise ValueError("contact_id needs to start with 'AC'")
         elif self.contact_type.value == "physical" and not self.is_verified:
@@ -38,3 +38,50 @@ class AlienContact(BaseModel):
             raise ValueError(
                 "Strong signals (> 7.0) should include received messages"
                 )
+        return self
+
+
+def display_alien_contact(alien_contact: AlienContact) -> None:
+    print(f"ID: {alien_contact.contact_id}")
+    print(f"Type: {alien_contact.contact_type}")
+    print(f"Location: {alien_contact.location}")
+    print(f"Signal: {alien_contact.signal_strength}/10")
+    print(f"Witnesses: {alien_contact.witness_count}")
+    if alien_contact.message_received is not None:
+        print(f"Notes: {alien_contact.message_received}")
+    else:
+        print()
+    print("================================")
+
+
+if __name__ == "__main__":
+    valid_contact = AlienContact(
+        contact_id="AC_2024_001",
+        type=ContactType.RADIO,
+        location="Area 51, Nevada",
+        signal_strength=8.5,
+        duration_minutes=92.3,
+        message_received="Greetings from Zeta Reticuli",
+        timestamp=datetime(2026, 8, 1),
+        is_verified=True
+    )
+    valid_contact.validate_data()
+    print("Space alien_contact data validation")
+    print("===================================")
+    display_alien_contact(valid_contact)
+    try:
+        unvalid_contact = AlienContact(
+                contact_id="B_2024_001",
+                type=ContactType.PHYSICAL,
+                location="Desert, Arakis",
+                signal_strength=8.5,
+                duration_minutes=1000,
+                message_received="Greetings from Paul",
+                timestamp=datetime(2026, 8, 1)
+            )
+        unvalid_contact.validate_data()
+    except ValueError as e:
+        print("Expected validation error:")
+        for error in e.errors():
+            variable_name = error["loc"][0]
+            print(f"{variable_name}: {error['msg']}")
