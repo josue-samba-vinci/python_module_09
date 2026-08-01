924f46afcff1842b4585dd1c4d0a5cc96934d9eb
Josue Samba Dianzungu <josuesamba99@gmail.com>
2026-08-01 17:19:04 +0200

    validate_data with modl_validator

--- ex1/alien_contact.py
+++ ex1/alien_contact.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from pydantic import BaseModel, Field, ValidationError
+from pydantic import BaseModel, Field, model_validator, ValidationError
 from datetime import datetime
 from typing import Optional
 
@@ -22,4 +22,19 @@ class AlienContact(BaseModel):
     message_received: Optional[str] = Field(default=None, max_length=500)
     is_verified: bool = False
 
-    
+    @model_validator(mode='after')
+    def validate_data(self) -> bool:
+        if not self.contact_id.startswith("AC"):
+            raise ValueError("contact_id needs to start with 'AC'")
+        elif self.contact_type.value == "physical" and not self.is_verified:
+            raise ValueError("Physical contact reports must be verified")
+        elif (self.contact_type.value == "telepathic"
+              and self.witness_count < 3):
+            raise ValueError(
+                "Telepathic contact requires at least 3 witnesses"
+                )
+        elif (self.signal_strength > 7
+              and not self.message_received):
+            raise ValueError(
+                "Strong signals (> 7.0) should include received messages"
+                )
