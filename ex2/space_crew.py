581965aa194f2b2d6a0afa0e9e1d8239985aef10
Josue Samba Dianzungu <josuesamba99@gmail.com>
2026-08-02 15:01:23 +0200

    CrewMember Base Model

--- ex2/space_crew.py
+++ ex2/space_crew.py
@@ -10,3 +10,13 @@ class Rank(Enum):
     LIEUTENANT = "lieutenant"
     CAPTAIN = "captain"
     COMMANDER = "commander"
+
+
+class CrewMember(BaseModel):
+    member_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=2, max_length=50)
+    rank: Rank
+    age: int = Field(ge=18, le=80)
+    specialization: str = Field(min_length=3, max_length=30)
+    years_experience: int = Field(ge=0, le=50)
+    is_active: bool = True
