f5b73edcbb1583afd624bf1dacba29b308993657
Josue Samba Dianzungu <josuesamba99@gmail.com>
2026-08-02 15:09:18 +0200

    SpaceMission Base Model

--- ex2/space_crew.py
+++ ex2/space_crew.py
@@ -20,3 +20,14 @@ class CrewMember(BaseModel):
     specialization: str = Field(min_length=3, max_length=30)
     years_experience: int = Field(ge=0, le=50)
     is_active: bool = True
+
+
+class SpaceMission(BaseModel):
+    mission_id: str = Field(min_length=5, max_length=15)
+    mission_name: str = Field(min_length=3, max_length=100)
+    destination: str = Field(min_length=3, max_length=50)
+    launch_date: datetime
+    duration_days: int = Field(ge=1, le=3650)
+    crew: list[CrewMember] = Field(min_length=1, max_length=12)
+    mission_status: str = "planned"
+    budget_millions: float = Field(ge=1, le=10000)
